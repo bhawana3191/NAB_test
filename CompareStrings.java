5e4a2cbb1d551598bac9be97f778cc4e3f9154c1
bhawana3191 <61607742+bhawana3191@users.noreply.github.com>
2020-03-03 11:58:11 +1100

    Update CompareStrings.java

--- CompareStrings.java
+++ CompareStrings.java
@@ -1,16 +1,18 @@
 import java.util.*;
+
 import java.util.Scanner;
 
-public class CompareStrings {
+
+public class Main {
+
     public static void main(String[] args) {
         String s1 = "a?c";
         String s2 = "??b";
+        String q1 = "";
+        String q2 = "";
         int count = 0;
         int min = 0;
         int max = 0;
-        String q1 = "";
-        String q2 = "";
-        
         Scanner inp = new Scanner(System.in);
         int tcNumber = inp.nextInt();
         int stringInput = (2 * tcNumber);
@@ -18,30 +20,30 @@ public class CompareStrings {
         for (int i = 0; i < stringInput; i++) {
             s[i] = inp.next();
         }
-           for (int i = 0; i < stringInput; i+= 2) {
-                q1 = s[i];
-               //System.out.println(q1);
-           }
-               for (int k = 1; k < stringInput; k+= 2) {
+        for (int i = 0; i < tcNumber; i += 2) {
+            q1 = s[i];
+            
+            for (int k = 1; k < tcNumber; k += 2) {
                 q2 = s[k];
-               
-                //System.out.println(q2);
-               }
-               
-               for (int j = 0; j < q1.length() ; j++)
-                 {
-     	            char c1 = q1.charAt(j);
-     	          char c2 = q2.charAt(j);
+                
+                for (int j = 0; j < q1.length(); j++)
+                {
 
+                    char c1 = q1.charAt(j);
+                    char c2 = q2.charAt(j);
                     if (c1 != c2 && c1 != '?' && c2 != '?') {
-                            min++;
-                            }
-                        if (c1 == '?' || c2 == '?') {
-                            count++;
-                        }
-                 }
-    
-       max = min + count;
-      System.out.println(min + " " + max);
+                        min++;
+                    }
+                    if (c1 == '?' || c2 == '?') {
+                        count++;
+                    }
+                }
+                max = min + count;
+                System.out.println(min + " " + max);
+            }
+
+        }
+
     }
+
 }
